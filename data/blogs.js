97825b849f2168967f1099fb575b3dbd974949fc
flowrax <jouw@email.nl>
2025-12-19 12:18:18 +0330

    Blog stukken toegevoegd

--- data/blogs.js
+++ data/blogs.js
@@ -23,7 +23,11 @@ window.blogs = [
     photos: ['img/2/foto1.jpg','img/2/foto2.jpg','img/2/foto3.jpg','img/2/foto4.jpg'],
     description: 'De laatste loodjes.',
     content: `<h2>Thorn - Nederland</h2>
-              <p>Na het nodige loodgieterswerk is het watersysteem niet meer lek...</p>`,
+              <p>Na het nodige loodgieterswerk is het watersysteem niet meer lek. Dit duurde natuurlijk langer dan gepland dus gezocht naar de volgende overnachtingsplek zo dicht mogelijk bij de grens. Dit werd het pittoreske Thorns. Om ‘s ochtends zo snel mogelijk weer weg te gaan.
+
+De laatste benodigdheden hebben we in Roermond gehaald. Waarbij we meteen de plooifietsen hebben kunnen uittesten. De score is prachtig blauw schoeisel geworden! Crocs kunnen natuurlijk niet ontbreken bij de “echte” kampeerder…
+
+Oja, ook (eindelijk) de grens over!</p>`,
     lat: 51.1931,
     lng: 5.9071
   },
@@ -35,7 +39,7 @@ window.blogs = [
     image: 'img/3/foto1.jpg',
     photos: ['img/3/foto1.jpg','img/3/foto2.jpg','img/3/foto3.jpg','img/3/foto4.jpg'],
     description: 'KAAL!',
-    content: `<h2>Forst - Duitsland</h2><p>Duitsland en Oostenrijk waren niet echt het benoemen waardig...</p>`,
+    content: `<h2>Forst - Duitsland</h2><p>Duitsland en Oostenrijk waren niet echt het benoemen waardig. Dus bij deze het proces van de verloren haren.</p>`,
     lat: 51.1594,
     lng: 14.6353
   },
@@ -47,7 +51,11 @@ window.blogs = [
     image: 'img/4/foto1.jpg',
     photos: ['img/4/foto1.jpg','img/4/foto2.jpg','img/4/foto3.jpg','img/4/foto4.jpg','img/4/foto5.jpg','img/4/foto6.jpg','img/4/foto7.jpg','img/4/foto8.jpg','img/4/foto9.jpg'],
     description: 'Hiken in de Dolomieten.',
-    content: `<h2>Auronzo Di Cadore - Italië</h2><p>De eerste geplande stop, de Dolomieten!...</p>`,
+    content: `<h2>Auronzo Di Cadore - Italië</h2><p>De eerste geplande stop, de Dolomieten! Na een prachtige hike van een kleine 10 kilometer de berg op bereikten we het uitzichtpunt bij de beroemde “drie pieken”. De winter is natuurlijk net afgelopen, maar op 2500m hoogte lag er nog een behoorlijk pak sneeuw. Het enige nadeel is dat we de 10 km ook weer naar beneden moesten… 
+
+Aangezien we volledig off-season zitten waren de pizzeria’s in de nabijgelegen (toeristen)dorpjes, tot Kimmia’s verdriet, allemaal gesloten. 
+
+Oja, Willem’s haar is er af. Hahaha!</p>`,
     lat: 46.5933,
     lng: 12.3142
   },
@@ -59,7 +67,9 @@ window.blogs = [
     image: 'img/5/foto1.jpg',
     photos: ['img/5/foto1.jpg','img/5/foto2.jpg','img/5/foto3.jpg'],
     description: 'Pizza, pizza en nog eens PIZZA!',
-    content: `<h2>Tarcento - Italië</h2><p>Er is eigenlijk maar één woord dat past bij deze stap: PIZZA!...</p>`,
+    content: `<h2>Tarcento - Italië</h2><p>Er is eigenlijk maar één woord dat past bij deze stap: PIZZA! Kimmia heeft na maanden voorpret eindelijk haar echte Italiaanse pizza. Zo blij zie ik haar zelden 😉.
+
+We stonden met de camper langs een helder blauw riviertje in een klein dorpje. Vooral de eigenaresse van de pizzeria was hilarisch en kon natuurlijk niet ontbreken op de foto.</p>`,
     lat: 46.3046,
     lng: 13.0423
   },
@@ -71,7 +81,13 @@ window.blogs = [
     image: 'img/6/foto1.jpg',
     photos: ['img/6/foto1.jpg','img/6/foto2.jpg','img/6/foto3.jpg','img/6/foto4.jpg','img/6/foto5.jpg','img/6/foto6.jpg','img/6/foto7.jpg','img/6/foto8.jpg','img/6/foto9.jpg','img/6/foto10.jpg'],
     description: 'Wat een verrassing.',
-    content: `<h2>Ljubljana - Slovenië</h2><p>En al weer door naar het volgende land...</p>`,
+    content: `<h2>Ljubljana - Slovenië</h2><p>En al weer door naar het volgende land, Slovenië!
+
+Beiden hadden we nul verwachtingen en wisten eigenlijk niets over Slovenië. Om die reden zijn we vlak bij de hoofdstad, Ljubljana, gaan staan om de volgende dag een Free Walking Tour te doen. 
+
+Het weer was wederom prachtig en de geschiedenis van het land (en de stad) waren ontzettend interessant. Blijkbaar is de halve stad ontworpen door Jože Plečnik. Leipe baas.
+
+Het eten was daarentegen precies wat we verwacht hadden. Typisch Slavisch waarbij de aardappel de hoofdrol vertolkt.</p>`,
     lat: 46.0569,
     lng: 14.5058
   },
@@ -83,7 +99,13 @@ window.blogs = [
     image: 'img/7/foto1.jpg',
     photos: ['img/7/foto1.jpg','img/7/foto2.jpg','img/7/foto3.jpg','img/7/foto4.jpg','img/7/foto5.jpg'],
     description: 'Terug in de tijd.',
-    content: `<h2>Primorsko-goranska - Kroatië</h2><p>Terug in de tijd! Nacht 1 en 2 in Kroatië...</p>`,
+    content: `<h2>Primorsko-goranska - Kroatië</h2><p>Terug in de tijd! Nacht 1 en 2 in Kroatië hebben wij gespendeerd bij een ontzettend lieve, gastvrije vrouw in de buurt van Rijeka. 
+
+Deze dame en haar hond(je) verwelkomt sinds het overlijden van haar man mensen op haar grond voor gezelschap. 
+
+Ze kookt nog op hout en houdt zich absoluut niet bezig met 21e eeuwse zaken.
+
+Er stond hier ook een ander jong stel, eindelijk! Met ongeveer hetzelfde reisplan als dat wij hebben. Erg leuk om wat gekletst te hebben met hen.</p>`,
     lat: 45.3297,
     lng: 14.4428
   },
@@ -95,7 +117,9 @@ window.blogs = [
     image: 'img/8/foto1.jpg',
     photos: ['img/8/foto1.jpg'],
     description: 'Tussen de olijven.',
-    content: `<h2>Sibenik - Kroatië</h2><p>Nacht 3 in Kroatië! Midden tussen de olijfbomen...</p>`,
+    content: `<h2>Sibenik - Kroatië</h2><p>Nacht 3 in Kroatië! Midden tussen de olijfbomen. Verder bijzonder weinig meegemaakt maar de weg naar deze plek toe was aardig krap met de bus.
+
+Koken met dit uitzicht is wel genieten!</p>`,
     lat: 43.7384,
     lng: 15.9052
   },
@@ -107,7 +131,11 @@ window.blogs = [
     image: 'img/9/foto1.jpg',
     photos: ['img/9/foto1.jpg','img/9/foto2.jpg','img/9/foto3.jpg','img/9/foto4.jpg','img/9/foto5.jpg','img/9/foto6.jpg','img/9/foto7.jpg','img/9/foto8.jpg','img/9/foto9.jpg','img/9/foto10.jpg','img/9/foto11.jpg','img/9/foto12.jpg'],
     description: 'Openluchtmuseum en aan het strand.',
-    content: `<h2>Dubrovnik - Kroatië</h2><p>Nacht 4 op een ontzettend fijne plek...</p>`,
+    content: `<h2>Dubrovnik - Kroatië</h2><p>Nacht 4 op een ontzettend fijne plek. De bus stond bijna aan het strand. Kimmia heeft zelfs een frisse duik genomen. En het uitzicht bij het wakker worden was geweldig (zie de foto uit de bus).
+
+De volgende dag zijn we naar Dubrovnik geweest. Een bizar mooie stad. Het enige nadeel, en daar deden wij vrolijk aan mee, het aantal toeristen. Desondanks een super interessante tour gehad van een local. Free walking tours zijn een dikke aanrader in elke stad. En voor de fans, een gedeelte van Game of Thrones is hier opgenomen. 
+
+Na dit openluchtmuseum zijn we ‘s middags gauw op pad gegaan richting Montenegro.</p>`,
     lat: 42.6507,
     lng: 18.0944
   },
@@ -119,7 +147,9 @@ window.blogs = [
     image: 'img/10/foto1.jpg',
     photos: ['img/10/foto1.jpg','img/10/foto2.jpg','img/10/foto3.jpg','img/10/foto4.jpg'],
     description: 'Een nachtje in Montenegro.',
-    content: `<h2>Kuside - Montenegro</h2><p>Montenegro was kort maar krachtig...</p>`,
+    content: `<h2>Kuside - Montenegro</h2><p>Montenegro was kort maar krachtig. Water bijgevuld. Geslapen langs een meertje en weer dooooorrrrrr.
+
+Op de terugweg zullen we hier zeker nog uitgebreider verblijven. Het was prachtig om doorheen te rijden.</p>`,
     lat: 42.7554,
     lng: 19.2519
   },
@@ -131,7 +161,9 @@ window.blogs = [
     image: 'img/11/foto4.jpg',
     photos: ['img/11/foto1.jpg','img/11/foto2.jpg','img/11/foto3.jpg','img/11/foto4.jpg'],
     description: 'Grenscontrole...',
-    content: `<h2>Krusevac - Servië</h2><p>Uitgebreide grenscontrole...</p>`,
+    content: `<h2>Krusevac - Servië</h2><p>Uitgebreide grenscontrole, gestopt door politie, nog een keer gestopt door politie en een nog uitgebreidere grenscontrole waarbij ze zelfs met een xray apparaatje naar drugs(?) hebben gezocht. Kortom bijzonder korte maar matige ervaring met Servië. 
+
+Wel op een mooi plekje bij een groot meer geslapen. En Kimmia heeft weer met honden kunnen knuffelen!</p>`,
     lat: 43.5804,
     lng: 21.3333
   },
@@ -143,7 +175,13 @@ window.blogs = [
     image: 'img/12/foto1.jpg',
     photos: ['img/12/foto1.jpg','img/12/foto2.jpg','img/12/foto3.jpg','img/12/foto4.jpg','img/12/foto5.jpg','img/12/foto6.jpg','img/12/foto7.jpg','img/12/foto8.jpg'],
     description:  'Harde wind tijdens het wandelen.',
-    content: `<h2>Panichishte - Bulgarije</h2><p>De eerste stop in Bulgarije...</p>`,
+    content: `<h2>Panichishte - Bulgarije</h2><p>De eerste stop in Bulgarije! Ten zuiden van Sofia zijn we gestopt bij het nationale park van Rila, bekend om haar 7 meren. Daar zijn we natuurlijk ook heen gewandeld. Er was een lift naar boven maar daar waren we te stoer (skeer) voor.
+
+De uitzichten waren prachtig, maar het was ontzettend koud en het waaide heel hard. Onze vermoeide beentjes hoopte op de lift naar beneden, maar door het slechte weer stond die helaas stil. Dus zijn we, haast rennend om de regen voor te blijven, naar beneden gewandeld. Al met al veel te gek gedaan natuurlijk.
+
+In de buurt van onze overnachtingsplaats was een hele lieve hond die natuurlijk gefotografeerd moest worden. En, iets dieper in het bos, een verlaten hotel. Vrij creepy en een goed begin van een horrorfilm.
+
+Kortom, heerlijk plekje :).</p>`,
     lat: 42.0187,
     lng: 23.6155
   },
@@ -155,7 +193,11 @@ window.blogs = [
     image: 'img/13/foto1.jpg',
     photos: ['img/13/foto1.jpg','img/13/foto2.jpg','img/13/foto3.jpg','img/13/foto4.jpg'],
     description: 'Poke-Bowl!',
-    content: `<h2>Kurtovo Konare - Bulgarije</h2><p>Plekje 2 in Bulgarije...</p>`,
+    content: `<h2>Kurtovo Konare - Bulgarije</h2><p>Plekje 2 in Bulgarije. Nog aardig moe van de hike hebben we een dagje rustig aan gedaan. 
+
+We stonden in de buurt van een dorpje tussen de rivier en de dijk. Mooi beschut tussen de struiken. Rondom te bus scharrelden veel dieren en ‘s avonds kregen we zelfs een heuse lichtshow van een hele hoop vuurvliegjes. Het is niet ontzettend goed te zien op de foto/video maar de kleine licht flitsjes zijn de kevertjes. (Blijkbaar zijn vuurvliegjes geen vliegen maar kevers).
+
+Het is ook zowaar gelukt om tofu te vinden dus de poke-bowl was een groot succes!</p>`,
     lat: 42.0083,
     lng: 25.3450
   },
@@ -167,7 +209,11 @@ window.blogs = [
     image: 'img/14/foto1.jpg',
     photos: ['img/14/foto1.jpg','img/14/foto2.jpg','img/14/foto3.jpg','img/14/foto4.jpg','img/14/foto5.jpg','img/14/foto6.jpg','img/14/foto7.jpg','img/14/foto8.jpg','img/14/foto9.jpg','img/14/foto10.jpg'],
     description: 'De oudste stad van Europa.',
-    content: `<h2>Plovdiv - Bulgarije</h2><p>Dagje naar Plovdiv...</p>`,
+    content: `<h2>Plovdiv - Bulgarije</h2><p>Dagje naar Plovdiv (geweldige naam voor een stad). 
+
+Plovdiv is een van de oudste steden van Europa. Al ruim 8000(!) jaar wonen hier mensen. Mogelijk was ik door dit feitje een beetje bevooroordeeld en viel het daarom vies tegen. Een aantal prachtige gebouwen en interessante plekken maar verder was dit geen hele bijzondere stad. Leuk om geweest te zijn, maar een middagje was voldoende. 
+
+Vanavond vlak voor de grens overnachten en morgen naar Turkije! Daar gaan we iets langer de tijd nemen om het land te ontdekken.</p>`,
     lat: 42.1354,
     lng: 24.7453
   },
@@ -179,7 +225,11 @@ window.blogs = [
     image: 'img/15/foto1.jpg',
     photos: ['img/15/foto1.jpg','img/15/foto2.jpg','img/15/foto3.jpg','img/15/foto4.jpg'],
     description: 'Eerste nacht Turkije.',
-    content: `<h2>Tekirdağ - Turkije</h2><p>Eerste overnachtingsplekje in Turkije...</p>`,
+    content: `<h2>Tekirdağ - Turkije</h2><p>Eerste overnachtingsplekje in Turkije. Zuidelijk van Istanboel en natuurlijk aan zee. 
+
+We werden meteen verwelkomt door de locals met een heerlijk bordje vlees en groenten van de BBQ. Een leuk gesprekje met handen en voeten want Engels was lastig. Maar precies de Turkse gastvrijheid die zo prettig is!
+
+En natuurlijk weer een (ontzettend dikke) hond. We hebben hier twee nachtjes gestaan om een zeer noodzakelijk handwasje te doen. We waren hier wat langer om te proberen een visum voor Turkmenistan te bemachtigen. Spoiler alert, nog niet gelukt…</p>`,
     lat: 40.9781,
     lng: 27.5156
   },
@@ -191,7 +241,9 @@ window.blogs = [
     image: 'img/16/foto1.jpg',
     photos: ['img/16/foto1.jpg','img/16/foto2.jpg'],
     description: 'Kanonnen!',
-    content: `<h2>Seddülbahir Köyü - Turkije</h2><p>Overnachtingsplekje twee...</p>`,
+    content: `<h2>Seddülbahir Köyü - Turkije</h2><p>Overnachtingsplekje twee. Wederom aan de kust. Na een nogal hobbelige, onverharde weg stonden we midden in een oude verdedigingslinie uit WO1. Dit was verder natuurlijk totaal niet onderhouden, en we konden er dus ook geen verdere informatie over vinden. Dus als resultaat zijn we lekker wezen rondneuzen natuurlijk haha.
+
+Hier hebben we ook 2 nachtjes gestaan, al werden we de 2e nacht wel goed wakker gehouden door wat locals met een uitgebreide draaitafel/disco-set😴</p>`,
     lat: 39.9698,
     lng: 26.3005
   },
@@ -203,7 +255,9 @@ window.blogs = [
     image: 'img/17/foto1.jpg',
     photos: ['img/17/foto1.jpg','img/17/foto2.jpg','img/17/foto3.jpg','img/17/foto4.jpg','img/17/foto5.jpg','img/17/foto6.jpg','img/17/foto7.jpg','img/17/foto8.jpg','img/17/foto9.jpg'],
     description: 'Rare jongens die Romeinen!',
-    content: `<h2>Bergama - Turkije</h2><p>Vandaag hebben we rond gehobbeld...</p>`,
+    content: `<h2>Bergama - Turkije</h2><p>Vandaag hebben we rond gehobbeld door de oude Romeinse stad Pergamon bij Bergama. Ontzettend mooi om te zien en er waren nog niet zo mega veel toeristen te vinden. Ook hier was het moeilijk om goede informatie te vinden (of het was erg slecht vertaald naar het Engels), dus weer lekker rondsnuffelen over de oude Romeinse weggetjes en gebouwen. Misschien vind je nog een oude schat? (Nee, maar het idee is leuk).
+
+Vlakbij Bergama stonden we aan een mooi meer. Helaas werden we vlak voor het slapengaan aangevallen door ongeveer een miljard vliegjes die door onze horren heenkwamen. Daarnaast waren er straathondjes die voor de verandering niet moddervet waren 🥺. Kortom, niet ons favoriete campeerplekje. </p>`,
     lat: 39.1200,
     lng: 27.1800
   }
